1ca51c21ce0f16c23a39c2f76ad9dbe3a5842226
muskangupta001 <124023698+muskangupta001@users.noreply.github.com>
2025-06-30 17:27:27 +0530

    Update digital_diceTb.v
    ssd

--- digital_diceTb.v
+++ digital_diceTb.v
@@ -6,12 +6,14 @@ module tb_digital_dice;
     reg reset;
     reg btn; // Button signal
     wire [2:0] dice_out;
+    wire [6:0] seg;
 
     digital_dice_top uut (
         .clk(clk),
         .reset(reset),
         .btn(btn),
         .dice_out(dice_out)
+        .seg(seg)
     );
 
     always #50 clk = ~clk;  // 10 MHz clock
